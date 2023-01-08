ab2afc97674875a4b875d522d3c9d18e1142467b
Chikanma <chikanmaonuegbu@gmail.com>
2023-01-07 23:24:03 -0700

    checkpoint1

--- frontend/lifestone-react-ui/src/components/TimelineComponent.jsx
+++ frontend/lifestone-react-ui/src/components/TimelineComponent.jsx
@@ -1,16 +1,18 @@
+import React from "react";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import { Button, Modal } from "reactstrap";
 
 const Timeline = () => {
   return (
     <VerticalTimeline>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
+      {/* <VerticalTimelineElement
+        className="bg-light vertical-timeline-element--work"
         date="2011 - present"
-        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        iconStyle={{ background: "rgb(255, 50, 150)", color: "#fff" }}
       >
         <h3 className="vertical-timeline-element-title">Creative Director</h3>
         <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
@@ -18,11 +20,10 @@ const Timeline = () => {
           Creative Direction, User Experience, Visual Design, Project
           Management, Team Leading
         </p>
-      </VerticalTimelineElement>
+      </VerticalTimelineElement> */}
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        date="2010 - 2011"
-        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        iconStyle={{ background: "rgb(255, 50, 150)", color: "#fff" }}
       >
         <h3 className="vertical-timeline-element-title">Art Director</h3>
         <h4 className="vertical-timeline-element-subtitle">
--- frontend/lifestone-react-ui/src/screens/TimelineScreen.jsx
+++ frontend/lifestone-react-ui/src/screens/TimelineScreen.jsx
@@ -1,15 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  Button,
+  Modal,
+  Form,
+  FormGroup,
+  Input,
+  ModalHeader,
+  Label,
+  ModalBody,
+  FormText,
+  ModalFooter,
+} from "reactstrap";
 import Timeline from "../components/TimelineComponent";
 const TimelineScreen = () => {
   const { isAuth } = useSelector((state) => state.auth);
-  console.log("isAuth", isAuth);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [image, setImage] = useState("");
+
+//   const fileSelectedHandler = (e) => {
+//     setImage(e.target.files[0]);
+//     console.log(e.target.files[0]);
+//   };
+
+//   console.log("isAuth", isAuth);
+
+
   return (
     <div>
       <div className="ms-5">
         <h2 className="text-white ">Timeline screen</h2>
       </div>{" "}
       <Timeline />
+      <div className="container">
+        <Button
+          className="btn btn-light"
+          rounded
+          onClick={() => setModalOpen(true)}
+          style={{ borderRadius: "20px" }}
+        >
+          <h1>+</h1>
+        </Button>
+        <Modal isOpen={modalOpen} toggle={() => setModalOpen(false)}>
+          <ModalHeader>Add Lifestone</ModalHeader>
+          <ModalBody>
+            <Form
+              className="col-8 mx-auto"
+              // onSubmit={handleSubmit}
+              encType="multipart/form-data"
+            >
+              <FormGroup>
+                <Label type="text">Title</Label>
+                <Input placeholder="Title" required />
+              </FormGroup>
+              <FormGroup>
+                <Label type="text">Description</Label>
+                <Input placeholder="Description" required />
+                {/* <FormText>Please include the date at the beginning of the description</FormText> */}
+              </FormGroup>
+              <FormGroup>
+                <Label type="text">Upload Image</Label>
+                <br />
+                <Input
+                  className="file"
+                  type="file"
+                  name="image"
+                  accept="image/*"
+                //   onChange={fileSelectedHandler}
+                />
+              </FormGroup>
+              <Input type="submit" value="Submit" />
+            </Form>
+          </ModalBody>
+        </Modal>
+      </div>
     </div>
   );
 };
