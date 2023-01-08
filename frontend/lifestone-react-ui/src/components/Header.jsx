1434490e389720664262255b21452d3f5c6d4965
Chikanma <chikanmaonuegbu@gmail.com>
2023-01-07 22:07:51 -0700

    created timeline and searchbar

--- frontend/lifestone-react-ui/src/components/Header.jsx
+++ frontend/lifestone-react-ui/src/components/Header.jsx
@@ -4,8 +4,10 @@ import {
   NavbarBrand,
   Button,
   NavItem,
+  Nav,
   FormGroup,
   Form,
+  NavLink,
   Input,
 } from "reactstrap";
 import { useNavigate } from "react-router-dom";
@@ -13,14 +15,16 @@ import { useDispatch, useSelector } from "react-redux";
 import WalletIcon from "@mui/icons-material/Wallet";
 import shortLogoWithText from "../images/shortLogoWithText.png";
 import { updateLoginState } from "../actions/AuthActions";
-
 const SearchBar = () => {
   return (
-    <div>
-      <Form>
+    <div style={{ borderRadius: "25px" }}>
+      <Form style={{ backgroundColor: "#36454F", color: "white" }}>
         <FormGroup>
           <Input
             placeholder="Search"
+            className="placeholder"
+            style={{ color: "white", backgroundColor: "#36454F" }}
+
             //  onChange={}
           />
         </FormGroup>
@@ -39,46 +43,73 @@ const Header = () => {
     <div>
       (
       <div>
-        <Navbar light className="d-flex align-items-center">
-          <NavbarBrand href="/" style={{ color: "white" }} alt="Logo">
+        <Navbar className="d-flex align-items-center">
+          <NavbarBrand
+            onClick={() => {
+              dispatch(updateLoginState(false));
+              navigate("/");
+            }}
+            style={{ color: "white" }}
+            alt="Logo"
+          >
             <img
               src={shortLogoWithText}
               style={{ height: "60px" }}
               alt="Our beautiful logo"
             />
           </NavbarBrand>
-          <NavItem>
-            <SearchBar/>
-          </NavItem>
+
           {!isAuth ? (
-            <Button
-              color="pink-600"
-              className="mx-5"
-              style={{ backgroundColor: "#de3576" }}
-              onClick={() => {
-                dispatch(updateLoginState(true));
-                navigate("/timeline");
-                window.location.reload();
-              }}
-            >
-              <div className="text-white">
-                Connect to wallet &nbsp;
-                <WalletIcon />
-              </div>
-            </Button>
+            <NavItem>
+              <Button
+                color="pink-600"
+                className="mx-5"
+                style={{ backgroundColor: "#de3576" }}
+                onClick={() => {
+                  dispatch(updateLoginState(true));
+                  navigate("/timeline");
+                  window.location.reload();
+                }}
+              >
+                <div className="text-white">
+                  Connect to wallet &nbsp;
+                  <WalletIcon />
+                </div>
+              </Button>
+            </NavItem>
           ) : null}
           {isAuth ? (
-            <Button
-              color="pink-600"
-              className="mx-5"
-              style={{ backgroundColor: "#de3576" }}
-              onClick={() => {
-                dispatch(updateLoginState(false));
-                navigate("/");
-              }}
-            >
-              Log out
-            </Button>
+            <React.Fragment>
+              <div className="align-items-center">
+                <SearchBar />
+              </div>
+              <Nav className="align-items-center">
+                <NavItem>
+                  <NavLink href="#" className="text-white">
+                    My Wall
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#" className="text-white">
+                    TimeLine
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <Button
+                    color="pink-600"
+                    // className="mx-5"
+                    style={{ backgroundColor: "#de3576" }}
+                    onClick={() => {
+                      dispatch(updateLoginState(false));
+                      navigate("/");
+                    }}
+                    className="text-white"
+                  >
+                    Log out
+                  </Button>
+                </NavItem>
+              </Nav>
+            </React.Fragment>
           ) : null}
         </Navbar>
         <hr style={{ height: "5px", color: "white" }} />
--- frontend/lifestone-react-ui/src/screens/TimelineScreen.jsx
+++ frontend/lifestone-react-ui/src/screens/TimelineScreen.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { useSelector } from 'react-redux';
-
+import { useSelector } from "react-redux";
+import Timeline from "../components/TimelineComponent";
 const TimelineScreen = () => {
-    const { isAuth } = useSelector( state => state.auth );
-    console.log("isAuth", isAuth);
-    return(
-        <>
-            <h1>Timeline screen</h1>
-        </>
-    )
+  const { isAuth } = useSelector((state) => state.auth);
+  console.log("isAuth", isAuth);
+  return (
+    <div>
+      <div className="ms-5">
+        <h2 className="text-white ">Timeline screen</h2>
+      </div>{" "}
+      <Timeline />
+    </div>
+  );
 };
 
 export default TimelineScreen;
